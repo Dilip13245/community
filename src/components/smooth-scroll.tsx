63ccb77bcfa8f0dede3527fcbd1ad852ec4d17c1
Dilip Chaudhary <dilipkumarc@hyperlinkinfosystem.net.in>
2025-11-13 11:55:36 +0530

    c

--- src/components/smooth-scroll.tsx
+++ src/components/smooth-scroll.tsx
@@ -12,7 +12,7 @@ export function SmoothScroll({ children }: { children: React.ReactNode }) {
     if (!lenisRef.current) {
       lenisRef.current = new Lenis({
         duration: 1.2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
         orientation: 'vertical',
         gestureOrientation: 'vertical',
         smoothWheel: true,
