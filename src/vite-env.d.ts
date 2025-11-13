3fafca2cc74eba657612fe01d7e420e1c33fbc02
Dilip Chaudhary <dilipkumarc@hyperlinkinfosystem.net.in>
2025-11-13 11:53:16 +0530

    c

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,16 @@
 declare global {
   interface Window {
     particlesJS: any;
+    lenis?: any;
+  }
+}
+
+declare module '@studio-freight/lenis' {
+  export default class Lenis {
+    constructor(options?: any);
+    raf(time: number): void;
+    scrollTo(target: number | string | HTMLElement, options?: { immediate?: boolean }): void;
+    destroy(): void;
   }
 }
 
